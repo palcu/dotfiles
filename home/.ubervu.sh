376c3c9771eb71a425f361c3fa96ceeab5baee57
Alex Palcuie <alex.palcuie@ubervu.com>
2013-10-18 18:05:11 +0300

    push_testing alias

--- home/.ubervu.sh
+++ home/.ubervu.sh
@@ -2,4 +2,4 @@ alias ubvutest="/home/django/web/frontend/ubervu/manage.py test"
 alias ubvushell="/home/django/web/frontend/ubervu/manage.py shell"
 alias ubvuprod="ipython /home/django/web/frontend/ubvushell.py development -i"
 
-alias push_testing='./Users/alex/uberVU/thehole/uvscripts/push_in_testing.sh'
+alias push_testing='/Users/alex/uberVU/thehole/uvscripts/github/push_in_testing.sh'
