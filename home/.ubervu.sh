f866a6f59624ba97bb74fd2d20cdb749bbee2b97
Alex Palcuie <alex.palcuie@ubervu.com>
2013-10-21 12:03:07 +0000

    add git ignore and ubervu prod

--- home/.ubervu.sh
+++ home/.ubervu.sh
@@ -1,5 +1,5 @@
 alias ubvutest="/home/django/web/frontend/ubervu/manage.py test"
 alias ubvushell="/home/django/web/frontend/ubervu/manage.py shell"
-alias ubvuprod="ipython /home/django/web/frontend/ubvushell.py development -i"
+alias ubvuprod="ipython /home/django/web/frontend/ubvushell.py production-remote -i"
 
 alias push_testing='/Users/alex/uberVU/thehole/uvscripts/github/push_in_testing.sh'
