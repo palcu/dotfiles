aca4ab337922ca90a079df9dc9a69354a9cad170
Alex Palcuie <alex.palcuie@ubervu.com>
2013-10-21 18:50:19 +0300

    pull-request shorthand

--- home/.ubervu.sh
+++ home/.ubervu.sh
@@ -1,5 +1,8 @@
-alias ubvutest="/home/django/web/frontend/ubervu/manage.py test"
-alias ubvushell="/home/django/web/frontend/ubervu/manage.py shell"
-alias ubvuprod="ipython /home/django/web/frontend/ubvushell.py production-remote -i"
+alias ubvu_test="/home/django/web/frontend/ubervu/manage.py test"
+alias ubvu_shell="/home/django/web/frontend/ubervu/manage.py shell"
+alias ubvu_prod="ipython /home/django/web/frontend/ubvushell.py production-remote -i"
 
-alias push_testing='/Users/alex/uberVU/thehole/uvscripts/github/push_in_testing.sh'
+alias ubvu_pushtesting='/Users/alex/uberVU/thehole/uvscripts/github/push_in_testing.sh'
+
+$branch = git symbolic-ref HEAD | cut -d'/' -f3 | cut -d'-' -f1
+alias ubvu_pullrequest="hub pull-request -i $branch"
