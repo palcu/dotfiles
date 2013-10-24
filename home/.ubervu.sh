3162a938aa2e09d3ec16975b92d487af73c80911
Alex Palcuie <alex.palcuie@ubervu.com>
2013-10-24 09:29:16 +0000

    remove pull-request

--- home/.ubervu.sh
+++ home/.ubervu.sh
@@ -1,9 +1,5 @@
 alias ubvu_test="/home/django/web/frontend/ubervu/manage.py test"
 alias ubvu_shell="/home/django/web/frontend/ubervu/manage.py shell"
 alias ubvu_prod="ipython /home/django/web/frontend/ubvushell.py production-remote -i"
-
 alias ubvu_pushtesting='/Users/alex/uberVU/thehole/uvscripts/github/push_in_testing.sh'
 alias ubvu_harvest='python /home/django/web/frontend/ubervu/run_harvest.py'
-
-$branch=git symbolic-ref HEAD | cut -d'/' -f3 | cut -d'-' -f1
-alias ubvu_pullrequest="hub pull-request -i $branch"
