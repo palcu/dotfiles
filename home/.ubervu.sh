5d574a28186d73075cda62e9129f691ecd132244
Alex Palcuie <alex.palcuie@ubervu.com>
2013-12-02 12:07:41 +0000

    add supervisorctl alias to ubervu

--- home/.ubervu.sh
+++ home/.ubervu.sh
@@ -3,3 +3,5 @@ alias ubvu_shell="/home/django/web/frontend/ubervu/manage.py shell"
 alias ubvu_prod="ipython /home/django/web/frontend/ubvushell.py production-remote -i"
 alias ubvu_pushtesting='/Users/alex/uberVU/thehole/uvscripts/github/push_in_testing.sh'
 alias ubvu_harvest='python /home/django/web/frontend/ubervu/run_harvest.py'
+
+alias sup='supervisorctl'
