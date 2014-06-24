7d3b80fa157267e389a43d2c1104841828a06872
Alex Palcuie <alex.palcuie@hootsuite.com>
2014-06-24 14:01:54 +0300

    Add vagrant-manager cuz it is cool

--- home/.ubervu.sh
+++ home/.ubervu.sh
@@ -1,9 +1,9 @@
 alias ubvu_test="/home/django/web/frontend/ubervu/manage.py test"
 alias ubvu_shell="/home/django/web/frontend/ubervu/manage.py shell"
 alias ubvu_prod="ipython /home/django/web/frontend/ubvushell.py production-remote -i"
-alias ubvu_pushtesting='/Users/alex/uberVU/thehole/uvscripts/github/push_in_testing.sh'
+alias ubvu_pushtesting='~/uberVU/thehole/uvscripts/github/push_in_testing.sh'
 alias ubvu_harvest='python /home/django/web/frontend/ubervu/run_harvest.py'
 
 alias sup='supervisorctl'
-alias open_before_work='osascript /Users/alex/dotfiles/applescripts/open_before_work.applescript'
-alias quit_after_work='osascript /Users/alex/dotfiles/applescripts/quit_after_work.applescript'
+alias open_before_work='osascript ~/dotfiles/applescripts/open_before_work.applescript'
+alias quit_after_work='osascript ~/dotfiles/applescripts/quit_after_work.applescript'
