0cada82d75ed892d6f0209b51d6cb90275d5eb0b
Alex Palcuie <alex.palcuie@gmail.com>
2020-07-03 22:47:44 +0100

    Drop IPython by default

--- zshrc/python.zsh
+++ zshrc/python.zsh
@@ -1,9 +1,1 @@
 export PYTHONSTARTUP=$HOME/.pythonrc
-
-# Alias IPython by default
-if command -v ipython > /dev/null; then
-    alias python="ipython"
-fi
-if command -v ipython3 > /dev/null; then
-    alias python3="ipython3"
-fi
