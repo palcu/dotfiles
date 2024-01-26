5e51f2db87314fd1b32529286cf8f14ee93f59fe
Alex Palcuie <alex.palcuie@gmail.com>
2024-01-26 20:30:12 +0000

    Remove old .ubervu.sh

--- home/.ubervu.sh
+++
@@ -1,4 +0,0 @@
-alias ubvu_pushtesting='~/uberVU/thehole/uvscripts/github/push_in_testing.sh'
-alias sup='supervisorctl'
-alias open_before_work='osascript ~/dotfiles/applescripts/open_before_work.applescript'
-alias quit_after_work='osascript ~/dotfiles/applescripts/quit_after_work.applescript'
